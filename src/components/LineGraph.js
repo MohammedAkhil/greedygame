d2522e048d1e2f1802a949d841b57d550c26464f
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-23 04:22:10 +0530

    add LineGraph component for graph

---
+++ src/components/LineGraph.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { LineChart, Line, XAxis, YAxis } from "recharts";
+
+export default function LineGraph(props) {
+  const { data } = props;
+  return (
+    <LineChart width={500} height={300} data={data}>
+      <XAxis dataKey="timestamp" />
+      <YAxis dataKey="eCPM" />
+      <Line type="monotone" dataKey="eCPM" stroke="#8884d8" />
+    </LineChart>
+  );
+}
