c5a9d6b5608375094d3a5cd5fcd1c4c9df0c96db
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-23 18:48:51 +0530

    add styles and layout the components in App.js

--- src/App.js
+++ src/App.js
@@ -3,23 +3,33 @@ import api from "./api";
 import GamesTable from "./components/GamesTable";
 import LineGraph from "./components/LineGraph";
 import BasicDatePicker from "./components/BasicDatePicker";
+import { Grid, Card, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles({
+  root: {
+    padding: 24
+  },
+  grid: {},
+  gridItem: {
+    padding: 8
+  },
+  graph: {
+    padding: 24
+  },
+  errorCard: {
+    display: "flex"
+  },
+  errorContent: {
+    alignSelf: "center",
+    padding: 24
+  }
+});
 
 function App() {
+  const classes = useStyles();
   const [games, updateGames] = useState([]);
   const cached = useRef(null);
-
-  function filterDate(selectedRange) {
-    const startDate = new Date(selectedRange.startDate);
-    const endDate = new Date(selectedRange.endDate);
-    console.log(startDate, endDate);
-
-    const filteredGames = cached.current.filter(function(game) {
-      const date = new Date(game.timestamp);
-      return date >= startDate && date <= endDate;
-    });
-    console.log(filteredGames);
-    updateGames(filteredGames);
-  }
 
   useEffect(() => {
     (async function() {
@@ -41,6 +51,19 @@ function App() {
       };
     });
 
+  function filterDate(selectedRange) {
+    const startDate = new Date(selectedRange.startDate);
+    const endDate = new Date(selectedRange.endDate);
+    console.log(startDate, endDate);
+
+    const filteredGames = cached.current.filter(function(game) {
+      const date = new Date(game.timestamp);
+      return date >= startDate && date <= endDate;
+    });
+    console.log(filteredGames);
+    updateGames(filteredGames);
+  }
+
   async function fetchData() {
     try {
       const response = await api
@@ -53,6 +76,7 @@ function App() {
       }
     } catch (error) {
       console.log(error);
+      return [];
     }
   }
 
@@ -60,27 +84,42 @@ function App() {
     updateGames(cached.current);
   }
 
-  console.log(games);
   return (
-    <div className="App">
-      <BasicDatePicker
-        filterDate={filterDate}
-        onClickClear={onClickClearFilter}
-      />
-      {games.length !== 0 && (
-        <React.Fragment>
-          <br />
-          <br />
-          <br />
-          <br />
-          <GamesTable games={games} />
-          <br />
-          <br />
-          <LineGraph data={games} />
-        </React.Fragment>
-      )}
+    <div className={classes.root}>
+      <Grid container spacing={24} className={classes.grid}>
+        <Grid item xs className={classes.gridItem}>
+          <BasicDatePicker
+            filterDate={filterDate}
+            onClickClear={onClickClearFilter}
+          />
+        </Grid>
+        {games.length !== 0 ? (
+          <React.Fragment>
+            <Grid className={classes.graph} item xs>
+              <LineGraph data={games} />
+            </Grid>
+            <Grid item xs>
+              <GamesTable games={games} />
+            </Grid>
+          </React.Fragment>
+        ) : (
+          error()
+        )}
+      </Grid>
     </div>
   );
-}
 
+  function error() {
+    return (
+      <Card className={classes.errorCard}>
+        <div className={classes.errorContent}>
+          <Typography variant="h6">
+            No data present in the given range.
+          </Typography>
+          <Typography variant="h6">Try with a different range.</Typography>
+        </div>
+      </Card>
+    );
+  }
+}
 export default App;
