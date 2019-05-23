06dde56a0cab840b4727d58c8bbd8e41b44e2484
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-23 19:22:25 +0530

    Updates

---
+++ precache-manifest.40791b6344370b7f83781381f1a6a601.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abde98f50fec34e3ac75ba900e5a8e73",
+    "url": "/greedygame/index.html"
+  },
+  {
+    "revision": "ab7073ee386856a11f9a",
+    "url": "/greedygame/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "8ef0c82c65633a3143b6",
+    "url": "/greedygame/static/js/2.1ef1e0fa.chunk.js"
+  },
+  {
+    "revision": "ab7073ee386856a11f9a",
+    "url": "/greedygame/static/js/main.3d36d849.chunk.js"
+  },
+  {
+    "revision": "a6f42973bced13191607",
+    "url": "/greedygame/static/js/runtime~main.7076341a.js"
+  }
+]);
