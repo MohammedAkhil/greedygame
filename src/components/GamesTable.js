727e122614dbdd4311dcf78f93280d14b053cd37
Mohammed Akhil <akhilmohammed05@gmail.com>
2019-05-23 18:47:57 +0530

    fix sort ascending issue

--- src/components/GamesTable.js
+++ src/components/GamesTable.js
@@ -1,27 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
+import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
-import TableFooter from "@material-ui/core/TableFooter";
 import TablePagination from "@material-ui/core/TablePagination";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
-import TablePaginationActions from "./TablePaginationActions";
 import EnhancedTableHead from "./EnhancedTableHead";
-
-const styles = theme => ({
-  root: {
-    width: "100%",
-    marginTop: theme.spacing.unit * 3
-  },
-  table: {
-    minWidth: 1020
-  },
-  tableWrapper: {
-    overflowX: "auto"
-  }
-});
 
 function desc(a, b, orderBy) {
   if (b[orderBy] < a[orderBy]) {
@@ -49,88 +35,151 @@ function getSorting(order, orderBy) {
     : (a, b) => -desc(a, b, orderBy);
 }
 
-function GamesTable(props) {
-  const { classes, games: rows } = props;
-  const [rowsPerPage, updateRowsPerPage] = useState(5);
-  const [page, updatePage] = useState(0);
-  const [order, updateOrder] = useState("asc");
-  const [orderBy, updateOrderBy] = useState("calories");
+const styles = theme => ({
+  root: {
+    width: "100%",
+    marginTop: theme.spacing.unit * 3
+  },
+  table: {
+    minWidth: 1020
+  },
+  tableWrapper: {
+    overflowX: "auto"
+  }
+});
 
-  function handleChangePage(event, newPage) {
-    updatePage(newPage);
+class GamesTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      order: "asc",
+      orderBy: "calories",
+      selected: [],
+      data: props.games,
+      page: 0,
+      rowsPerPage: 5
+    };
   }
 
-  function handleChangeRowsPerPage(event) {
-    updatePage(0);
-    updateRowsPerPage(event.target.value);
-  }
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, rows.length - page * rowsPerPage);
-
-  const handleRequestSort = (event, property) => {
+  handleRequestSort = (event, property) => {
     const orderBy = property;
     let order = "desc";
 
-    if (orderBy === property && order === "desc") {
+    if (this.state.orderBy === property && this.state.order === "desc") {
       order = "asc";
     }
 
-    updateOrder(order);
-    updateOrderBy(orderBy);
+    this.setState({ order, orderBy });
   };
 
-  return (
-    <Paper className={classes.root}>
-      <div className={classes.tableWrapper}>
-        <Table className={classes.table}>
-          <EnhancedTableHead
-            order={order}
-            orderBy={orderBy}
-            onRequestSort={handleRequestSort}
-            rowCount={rows.length}
-          />
-          <TableBody>
-            {stableSort(rows, getSorting(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map(row => (
-                <TableRow key={row.id}>
-                  <TableCell component="th" scope="row">
-                    {row.game}
-                  </TableCell>
-                  <TableCell align="right">{row.timestamp}</TableCell>
-                  <TableCell align="right">{row.revenue}</TableCell>
-                  <TableCell align="right">{row.impressions}</TableCell>
-                  <TableCell align="right">{row.eCPM}</TableCell>
+  handleSelectAllClick = event => {
+    if (event.target.checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }));
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+  render() {
+    const { classes } = this.props;
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <EnhancedTableHead
+              numSelected={selected.length}
+              order={order}
+              orderBy={orderBy}
+              onSelectAllClick={this.handleSelectAllClick}
+              onRequestSort={this.handleRequestSort}
+              rowCount={data.length}
+            />
+            <TableBody>
+              {stableSort(data, getSorting(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map(n => {
+                  return (
+                    <TableRow
+                      hover
+                      onClick={event => this.handleClick(event, n.id)}
+                      tabIndex={-1}
+                      key={n.id}
+                    >
+                      <TableCell component="th" scope="row" padding="none">
+                        {n.game}
+                      </TableCell>
+                      <TableCell align="right">{n.timestamp}</TableCell>
+                      <TableCell align="right">{n.revenue}</TableCell>
+                      <TableCell align="right">{n.impressions}</TableCell>
+                      <TableCell align="right">{n.eCPM}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow style={{ height: 49 * emptyRows }}>
+                  <TableCell colSpan={6} />
                 </TableRow>
-              ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 48 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-          <TableFooter>
-            <TableRow>
-              <TablePagination
-                rowsPerPageOptions={[5, 10]}
-                colSpan={3}
-                count={rows.length}
-                rowsPerPage={rowsPerPage}
-                page={page}
-                SelectProps={{
-                  native: true
-                }}
-                onChangePage={handleChangePage}
-                onChangeRowsPerPage={handleChangeRowsPerPage}
-                ActionsComponent={TablePaginationActions}
-              />
-            </TableRow>
-          </TableFooter>
-        </Table>
-      </div>
-    </Paper>
-  );
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
+          rowsPerPageOptions={[5, 10]}
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            "aria-label": "Previous Page"
+          }}
+          nextIconButtonProps={{
+            "aria-label": "Next Page"
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+      </Paper>
+    );
+  }
 }
+
+GamesTable.propTypes = {
+  classes: PropTypes.object.isRequired
+};
 
 export default withStyles(styles)(GamesTable);
